5e96330cd003254dc676faba0b3802d7ddedab51
Kauã Queiroz <queiroz-dev@outlook.com>
2023-08-28 10:43:13 -0300

    Changing the Title, Logo and Background Color

--- Python/pygame/main.py
+++ Python/pygame/main.py
@@ -6,8 +6,18 @@ pygame.init()
 # Create the screen
 screen = pygame.display.set_mode((400, 300))
 
+# Title and Icon
+pygame.display.set_caption("Space Invaders")
+icon = pygame.image.load('ufo.png')
+pygame.display.set_icon(icon)
+
+# Game Loop
 running = True
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+
+    # RBG - Red, Green, Blue
+    screen.fill((255, 0, 0))
+    pygame.display.update()
