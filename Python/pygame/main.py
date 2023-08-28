bcf1f0e6ed04e9c56da7fe6fe6451123528874c3
Kauã Queiroz <queiroz-dev@outlook.com>
2023-08-28 09:24:50 -0300

    Creating Our First Game Window

---
+++ Python/pygame/main.py
@@ -0,0 +1,13 @@
+import pygame
+
+# Initialize the pygame
+pygame.init()
+
+# Create the screen
+screen = pygame.display.set_mode((400, 300))
+
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
