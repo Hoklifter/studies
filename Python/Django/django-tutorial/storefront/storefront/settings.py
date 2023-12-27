9f9c6255c2fa8515f5b60a448b191074ea666dee
Kauã Queiroz <queiroz-dev@outlook.com>
2023-12-27 13:21:17 -0300

    django-tutorial: Using Django Debug Toolbar

--- Python/Django/django-tutorial/storefront/storefront/settings.py
+++ Python/Django/django-tutorial/storefront/storefront/settings.py
@@ -12,6 +12,10 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 
 from pathlib import Path
 
+INTERNAL_IPS = [
+    "127.0.0.1",
+]
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -37,10 +41,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'playground'
+    'playground',
+    'debug_toolbar'
 ]
 
 MIDDLEWARE = [
+    "debug_toolbar.middleware.DebugToolbarMiddleware",
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
