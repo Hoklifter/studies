0fc704ed0ee43037402c17349efea97ad2f5eaea
Kauã Queiroz <queiroz-dev@outlook.com>
2024-01-18 18:55:24 -0300

    http-course-project: Normalize URLs

---
+++ JavaScript/HTTP Networking Course – Fetch and REST APIs in JavaScript/webcrawler-http/crawl.test.js
@@ -0,0 +1,23 @@
+const {normalizeURL} = require('./crawl.js')
+const {test, expect} = require('@jest/globals')
+
+test('normalizeURL strip protocol', () => {
+    const input = 'https://blog.boot.dev/path/'
+    const actual = normalizeURL(input)
+    const expected = 'blog.boot.dev/path'
+    expect(actual).toEqual(expected)
+})
+
+test('normalizeURL strip trailing slash', () => {
+    const input = 'https://blog.boot.dev/path/'
+    const actual = normalizeURL(input)
+    const expected = 'blog.boot.dev/path'
+    expect(actual).toEqual(expected)
+})
+
+test('normalizeURL strip http', () => {
+    const input = 'http://blog.boot.dev/path'
+    const actual = normalizeURL(input)
+    const expected = 'blog.boot.dev/path'
+    expect(actual).toEqual(expected)
+})
