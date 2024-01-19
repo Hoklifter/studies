acc4d4f51d16a55ea983238abe1ccad158b8f12e
Kauã Queiroz <queiroz-dev@outlook.com>
2024-01-19 13:23:54 -0300

    http-course-project: URLs from HTML

--- JavaScript/HTTP Networking Course – Fetch and REST APIs in JavaScript/webcrawler-http/crawl.js
+++ JavaScript/HTTP Networking Course – Fetch and REST APIs in JavaScript/webcrawler-http/crawl.js
@@ -1,3 +1,30 @@
+const {JSDOM} = require('jsdom')
+
+function getURLsFromHTML(htmlBody, baseURL) {
+    const urls = []
+    const dom = new JSDOM(htmlBody)
+    const linkElements = dom.window.document.querySelectorAll('a')
+    for (const linkElement of linkElements) {
+        if (linkElement.href.slice(0, 1) === '/') {
+            // relative
+            try {
+                const urlObj = new URL(`${baseURL}${linkElement.href}`)
+                urls.push(`${baseURL}${linkElement.href}`)
+            } catch(err) {
+                console.log(`error with relative url: ${err.message}`)
+            }
+        } else {
+            // absolute
+            try {
+                const urlObj = new URL(linkElement.href)
+                urls.push(linkElement.href)
+            } catch(err) {
+                console.log(`error with absolute url: ${err.message}`)
+            }
+        }
+    }
+    return urls
+}
 function normalizeURL(urlString) {
     const urlObj = new URL(urlString)
     const hostPath = `${urlObj.hostname}${urlObj.pathname}`
@@ -9,5 +36,6 @@ function normalizeURL(urlString) {
 
 
 module.exports = {
-    normalizeURL
+    normalizeURL,
+    getURLsFromHTML
 }
--- JavaScript/HTTP Networking Course – Fetch and REST APIs in JavaScript/webcrawler-http/crawl.test.js
+++ JavaScript/HTTP Networking Course – Fetch and REST APIs in JavaScript/webcrawler-http/crawl.test.js
@@ -1,4 +1,4 @@
-const {normalizeURL} = require('./crawl.js')
+const {normalizeURL, getURLsFromHTML} = require('./crawl.js')
 const {test, expect} = require('@jest/globals')
 
 test('normalizeURL strip protocol', () => {
@@ -15,9 +15,77 @@ test('normalizeURL strip trailing slash', () => {
     expect(actual).toEqual(expected)
 })
 
+test('normalizeURL capitals', () => {
+    const input = 'http://BLOG.boot.dev/path'
+    const actual = normalizeURL(input)
+    const expected = 'blog.boot.dev/path'
+    expect(actual).toEqual(expected)
+})
+
 test('normalizeURL strip http', () => {
     const input = 'http://blog.boot.dev/path'
     const actual = normalizeURL(input)
     const expected = 'blog.boot.dev/path'
+    expect(actual).toEqual(expected)
+})
+
+test('getURLsFromHTML absolute', () => {
+    const inputHTMLBody = `
+<html>
+    <body>
+        <a href="https://blog.boot.dev/path/">Boot.dev Blog</a>
+    </body>
+</html>
+`
+    const inputBaseURL = "https://blog.boot.dev"
+    const actual = getURLsFromHTML(inputHTMLBody, inputBaseURL)
+    const expected = ["https://blog.boot.dev/path/"]
+    expect(actual).toEqual(expected)
+})
+
+
+test('getURLsFromHTML relative', () => {
+    const inputHTMLBody = `
+    <html>
+    <body>
+    <a href="/path/">Boot.dev Blog</a>
+    </body>
+    </html>
+    `
+    const inputBaseURL = "https://blog.boot.dev"
+    const actual = getURLsFromHTML(inputHTMLBody, inputBaseURL)
+    const expected = ["https://blog.boot.dev/path/"]
+    expect(actual).toEqual(expected)
+})
+
+test('getURLsFromHTML both', () => {
+    const inputHTMLBody = `
+<html>
+    <body>
+        <a href="https://blog.boot.dev/path1/">Boot.dev Blog Path One</a>
+        <a href="/path2/">Boot.dev Blog Path Two</a>
+    </body>
+</html>
+`
+    const inputBaseURL = "https://blog.boot.dev"
+    const actual = getURLsFromHTML(inputHTMLBody, inputBaseURL)
+    const expected = [
+        "https://blog.boot.dev/path1/",
+        "https://blog.boot.dev/path2/"
+    ]
+    expect(actual).toEqual(expected)
+})
+
+test('getURLsFromHTML invalid', () => {
+    const inputHTMLBody = `
+    <html>
+    <body>
+    <a href="invalid">Invalid URL</a>
+    </body>
+    </html>
+    `
+    const inputBaseURL = "https://blog.boot.dev"
+    const actual = getURLsFromHTML(inputHTMLBody, inputBaseURL)
+    const expected = []
     expect(actual).toEqual(expected)
 })
