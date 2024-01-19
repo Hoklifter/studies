4f6145e7bdd6f3afacbb7e94f086af4639f2857e
Kauã Queiroz <queiroz-dev@outlook.com>
2024-01-19 15:49:01 -0300

    http-course-project: Using Fetch

--- JavaScript/HTTP Networking Course – Fetch and REST APIs in JavaScript/webcrawler-http/crawl.js
+++ JavaScript/HTTP Networking Course – Fetch and REST APIs in JavaScript/webcrawler-http/crawl.js
@@ -1,5 +1,25 @@
 const {JSDOM} = require('jsdom')
 
+async function crawlPage(currentURL) {
+    console.log(`actively crawling ${currentURL}`)
+    try {
+        const resp = await fetch(currentURL)
+        if (resp.status > 399) {
+            console.log(`error in fetch with status code: ${resp.status} on page ${currentURL}`)
+            return
+        }
+
+        const contentType = resp.headers.get("content-type")
+        if (!contentType.includes("text/html")){
+            console.log(`non html response, content type: ${contentType} on page: ${currentURL}`)
+            return
+        }
+        console.log(await resp.text())
+    } catch (err) {
+        console.log(`error in fetch: ${err.message}, on page ${currentURL}`)
+    }
+}
+
 function getURLsFromHTML(htmlBody, baseURL) {
     const urls = []
     const dom = new JSDOM(htmlBody)
@@ -37,5 +57,6 @@ function normalizeURL(urlString) {
 
 module.exports = {
     normalizeURL,
-    getURLsFromHTML
+    getURLsFromHTML,
+    crawlPage,
 }
