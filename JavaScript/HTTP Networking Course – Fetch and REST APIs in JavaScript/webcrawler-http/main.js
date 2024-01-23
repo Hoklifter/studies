a2f2caf651c0e5b461ba45dea1f53bcc48b6141f
Kauã Queiroz <queiroz-dev@outlook.com>
2024-01-23 11:03:35 -0300

    http-course-project: Print an SEO report

--- JavaScript/HTTP Networking Course – Fetch and REST APIs in JavaScript/webcrawler-http/main.js
+++ JavaScript/HTTP Networking Course – Fetch and REST APIs in JavaScript/webcrawler-http/main.js
@@ -1,4 +1,5 @@
 const {crawlPage} = require('./crawl.js')
+const {printReport} = require('./report.js')
 
 async function main() {
     if (process.argv.length < 3) {
@@ -12,10 +13,8 @@ async function main() {
     const baseURL = process.argv[2]
     console.log(`starting crawl of ${baseURL}`)
     const pages = await crawlPage(baseURL, baseURL, {})
+    printReport(pages)
 
-    for (const page of Object.entries(pages)) {
-        console.log(page)
-    }
 }
 
 main()
